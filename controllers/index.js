3345c1b2ee587453c3139389f1a3768a91ecbd29
unknown <danleviobello@gmail.com>
2023-09-11 16:11:19 -0500

    Worked on api routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoute = require('./home');
 
+router.use('/'. homeRoute);
 router.use('/api', apiRoutes);
 
 module.exports =router;
